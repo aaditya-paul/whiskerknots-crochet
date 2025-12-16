ab06f60812c7fef7848dd73d06dbfea4f3414c7f
aaditya-paul <aadityapaul2006.programmer@gmail.com>
2025-12-16 17:29:00 +0530

    chore: add @google/genai dependency to package.json

--- components/ChatAssistant.tsx
+++ components/ChatAssistant.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useRef, useEffect } from "react";
 import { MessageCircle, X, Send, Sparkles } from "lucide-react";
 import { GenerateContentResponse } from "@google/genai";
